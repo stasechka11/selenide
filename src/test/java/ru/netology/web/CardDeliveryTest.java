178e832fd1264b3dbfceabd93df468d3f640f391
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-02-17 11:23:48 +0400

    added shouldBeSuccessCardDeliveryOrder test

---
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -0,0 +1,37 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessCardDeliveryOrder() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        String planningDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Потапова Анна");
+        $("[data-test-id='phone'] input").setValue("+71230077389");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+
+        $("button.button .spin").shouldBe(Condition.disappear, Duration.ofSeconds(16));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                        .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+    }
+}
